e1a78c984acd25878bcbb757471ceef9108a12c4
Shekhar Patnaik <patnaikshekhar@gmail.com>
2019-05-25 21:59:38 +0100

    Hello Wodgy

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.end('Hello'))
+app.get('/', (req, res) => res.end('Hello Wodgy'))
 app.listen(8080, () => console.log('Server Started'))
