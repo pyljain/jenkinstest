de3bcaf62fb41807e6d0447557ca8b1beea44b51
Shekhar Patnaik <patnaikshekhar@gmail.com>
2019-05-27 00:06:38 +0100

    Updated text

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.end('Hello Wodgy'))
+app.get('/', (req, res) => res.end('Hello Wodgy with proper pipeline'))
 app.listen(8080, () => console.log('Server Started'))
